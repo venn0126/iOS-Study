2295699f4f982f04b68af2336b1a05ad466dc773
venn <niuwei@fosafer.com>
2019-10-08 23:48:38 +0800

    bak location

---
+++ 2019/1008/SimulateLocation/SimulateLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimulateLocation
+//
+//  Created by 牛威 on 2019/9/30.
+//  Copyright © 2019 fosafer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
