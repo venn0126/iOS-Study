d356a69fe75151192678890fb27cfdd4bd89a224
venn0126 <venn221@gmail.com>
2021-08-21 23:39:21 +0800

    Concurrency
    Add get image from URLSession

--- 2021/0821/ConcurrencyPlayground.playground/Contents.swift
+++ 2021/0821/ConcurrencyPlayground.playground/Contents.swift
@@ -9,3 +9,35 @@ func someNames() async -> [String] {
 
 print("concurreny begin")
 //let res = await someNames()
+
+//let res = URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://jsonplaceholder.typicode.com/posts/1")!))
+//print(res)
+
+
+/// Download image by URLSession and closure
+func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+    URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+}
+
+
+
+let url = URL(string: "https://github.com/venn0126/iOS-Study/blob/master/Resource/IMG_1258.jpeg?raw=true") ?? nil
+//let url = URL(string: "")
+guard let url = url else {
+    print("url is nil")
+    fatalError()
+}
+// get data func
+getData(from: url) { data, response, error in
+    guard let data = data, error == nil else { return }
+    print(response?.suggestedFilename ?? url.lastPathComponent)
+    print("Download Finished")
+    // always update the UI from the main thread
+    DispatchQueue.main.async() {
+        print("image len is \(data.count)")
+    }
+    
+}
+
+
+
