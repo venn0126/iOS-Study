d22dfd7487cf18b0ea44101b07f172dc884f4baa
venn0126 <venn221@gmail.com>
2021-08-26 01:06:08 +0800

    meet async/await

--- 2021/0821/ConcurrencyPlayground.playground/Contents.swift
+++ 2021/0821/ConcurrencyPlayground.playground/Contents.swift
@@ -150,8 +150,78 @@ let account = BankAccount()
 
 //print("The end")
 
+enum FetchError: Error {
+    case NotFound
+    case TypeError
+    case Unknown
+}
 
 
+func fetchThumbnail(for id: String,completion: @escaping (UIImage?, Error?) -> Void) {
+    let request = URLRequest(url: URL(string: id)!)
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            completion(nil,error)
+        } else if (response as? HTTPURLResponse)?.statusCode != 200 {
+            completion(nil,error)
+        } else {
+            guard let image = UIImage(data: data!) else {
+                completion(nil,error)
+                return
+            }
+            image.prepareThumbnail(of: CGSize(width: 40, height: 40)) { thumbnail in
+                
+                guard let thum = thumbnail else {
+                    completion(nil, error)
+                    return
+                }
+                completion(thum,nil)
+            }
+        }
+    }
+    task.resume()
+}
+
+func fetchThumbnailAsync(for id: String) async throws -> UIImage {
+    
+    let request = URLRequest(url: URL(string: id)!)
+    
+    let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+        throw FetchError.NotFound
+    }
+    let maybeImage = UIImage(data: data)
+    guard let thumbnail = await maybeImage?.thumbnail else {
+        throw FetchError.NotFound
+    }
+    
+    return thumbnail
+}
+
+
+
+
+extension UIImage {
+    
+    var thumbnail: UIImage? {
+        get async {
+            let size = CGSize(width: 40, height: 40)
+            return await self.byPreparingThumbnail(ofSize: size)
+        }
+    }
+    
+    func prepareThumbnail(of: CGSize,completion: (UIImage?) -> Void) {
+        
+        // size
+        
+        // draw
+    }
+}
+
+
+// adopting
+
+// snap
 
 
 
