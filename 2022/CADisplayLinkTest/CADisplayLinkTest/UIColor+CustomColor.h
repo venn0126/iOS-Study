2993806573edd1449e92099940c5dfe58971b7a1
weiniu <venn221@gmail.com>
2022-04-12 16:49:09 +0800

    add assets 0 and 1

---
+++ 2022/CADisplayLinkTest/CADisplayLinkTest/UIColor+CustomColor.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+CustomColor.h
+//  CADisplayLinkTest
+//
+//  Created by Augus on 2022/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (CustomColor)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2022/CADisplayLinkTest/CADisplayLinkTest/UIColor+CustomColor.m
@@ -0,0 +1,12 @@
+//
+//  UIColor+CustomColor.m
+//  CADisplayLinkTest
+//
+//  Created by Augus on 2022/4/12.
+//
+
+#import "UIColor+CustomColor.h"
+
+@implementation UIColor (CustomColor)
+
+@end
