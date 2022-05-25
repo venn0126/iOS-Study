d37175f83dfd7fac103888b6b6a83756fe15d5bd
weiniu <venn221@gmail.com>
2022-05-25 17:41:35 +0800

    add a macho png

---
+++ 2022/CADisplayLinkTest/CADisplayLinkTest/GaoTianView.h
@@ -0,0 +1,16 @@
+//
+//  GaoTianView.h
+//  CADisplayLinkTest
+//
+//  Created by Augus on 2022/5/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GaoTianView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2022/CADisplayLinkTest/CADisplayLinkTest/GaoTianView.m
@@ -0,0 +1,20 @@
+//
+//  GaoTianView.m
+//  CADisplayLinkTest
+//
+//  Created by Augus on 2022/5/24.
+//
+
+#import "GaoTianView.h"
+
+@implementation GaoTianView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
