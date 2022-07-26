468631a330874e22a3a1f0aca7fcac3e53688faf
weiniu <venn221@gmail.com>
2022-07-26 14:11:16 +0800

    update decoration md

---
+++ 2022/TestMutableDictionary/TestMutableDictionary/SNAugusCollectionViewFlowLayout.h
@@ -0,0 +1,16 @@
+//
+//  SNAugusCollectionViewFlowLayout.h
+//  TestMutableDictionary
+//
+//  Created by Augus on 2022/7/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SNAugusCollectionViewFlowLayout : UICollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2022/TestMutableDictionary/TestMutableDictionary/SNAugusCollectionViewFlowLayout.m
@@ -0,0 +1,12 @@
+//
+//  SNAugusCollectionViewFlowLayout.m
+//  TestMutableDictionary
+//
+//  Created by Augus on 2022/7/25.
+//
+
+#import "SNAugusCollectionViewFlowLayout.h"
+
+@implementation SNAugusCollectionViewFlowLayout
+
+@end
