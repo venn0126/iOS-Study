ecb00fea1ea7734fd7346ca1cc809fc45d2a2004
weiniu <venn221@gmail.com>
2022-11-30 22:19:00 +0800

    add：怎加oc引用swift的桥接文件

---
+++ 2022/TestMutableDictionary/TestMutableDictionary/GTPerson.h
@@ -0,0 +1,19 @@
+//
+//  GTPerson.h
+//  TestMutableDictionary
+//
+//  Created by Augus on 2022/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GTPerson : NSObject
+
+/// Persone's name
+@property (nonatomic, copy) NSString *name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2022/TestMutableDictionary/TestMutableDictionary/GTWidgetCenter.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  TestMutableDictionary
+//
+//  Created by Augus on 2022/11/30.
+//
+
+//import Foundation
+
