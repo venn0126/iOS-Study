5aac2deee3623c5f2d2a727745d5b8a71373f136
weiniu <weiniu@sohu-inc.com>
2023-08-14 00:36:46 +0800

    add: add unicode to utf8

---
+++ 2023/TestHookEncryption/TestHookEncryption/NSDictionary+Extension.h
@@ -0,0 +1,25 @@
+//
+//  NSDictionary+Extension.h
+//  TestHookEncryption
+//
+//  Created by Augus on 2023/8/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDictionary (Extension)
+
+
+
+@end
+
+
+@interface NSArray (Extension)
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2023/TestHookEncryption/TestHookEncryption/NSDictionary+Extension.m
@@ -0,0 +1,61 @@
+//
+//  NSDictionary+Extension.m
+//  TestHookEncryption
+//
+//  Created by Augus on 2023/8/13.
+//
+
+#import "NSDictionary+Extension.h"
+
+@implementation NSDictionary (Extension)
+
+
+- (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level
+{
+    NSMutableString *mStr = [NSMutableString string];
+    NSMutableString *tab = [NSMutableString stringWithString:@""];
+    for (int i = 0; i < level; i++) {
+        [tab appendString:@"\t"];
+    }
+    [mStr appendString:@"{\n"];
+    NSArray *allKey = self.allKeys;
+    for (int i = 0; i < allKey.count; i++) {
+        id value = self[allKey[i]];
+        NSString *lastSymbol = (allKey.count == i + 1) ? @"":@";";
+        if ([value respondsToSelector:@selector(descriptionWithLocale:indent:)]) {
+            [mStr appendFormat:@"\t%@%@ = %@%@\n",tab,allKey[i],[value descriptionWithLocale:locale indent:level + 1],lastSymbol];
+        } else {
+            [mStr appendFormat:@"\t%@%@ = %@%@\n",tab,allKey[i],value,lastSymbol];
+        }
+    }
+    [mStr appendFormat:@"%@}",tab];
+    return mStr;
+}
+
+
+@end
+
+@implementation NSArray (Extension)
+
+- (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level
+{
+    NSMutableString *mStr = [NSMutableString string];
+    NSMutableString *tab = [NSMutableString stringWithString:@""];
+    for (int i = 0; i < level; i++) {
+        [tab appendString:@"\t"];
+    }
+    [mStr appendString:@"(\n"];
+    for (int i = 0; i < self.count; i++) {
+         NSString *lastSymbol = (self.count == i + 1) ? @"":@",";
+        id value = self[i];
+        if ([value respondsToSelector:@selector(descriptionWithLocale:indent:)]) {
+            [mStr appendFormat:@"\t%@%@%@\n",tab,[value descriptionWithLocale:locale indent:level + 1],lastSymbol];
+        } else {
+            [mStr appendFormat:@"\t%@%@%@\n",tab,value,lastSymbol];
+        }
+    }
+    [mStr appendFormat:@"%@)",tab];
+    return mStr;
+}
+
+@end
