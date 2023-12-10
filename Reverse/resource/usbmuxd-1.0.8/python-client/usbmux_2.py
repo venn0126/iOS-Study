cad9f70d3188c796648a032d2b0fa03c0f44de67
augus <venn221@gmail.com>
2023-12-11 02:04:09 +0800

    add: add tcprelay for py3 support

--- Reverse/resource/usbmuxd-1.0.8/python-client/usbmux_2.py
+++ Reverse/resource/usbmuxd-1.0.8/python-client/usbmux_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 #	usbmux.py - usbmux client library for Python
@@ -236,11 +236,11 @@ class USBMux(object):
 
 if __name__ == "__main__":
 	mux = USBMux()
-	print ("Waiting for devices...")
+	print "Waiting for devices..."
 	if not mux.devices:
 		mux.process(0.1)
 	while True:
-		print ("Devices:")
+		print "Devices:"
 		for dev in mux.devices:
-			print( dev)
+			print dev
 		mux.process()
