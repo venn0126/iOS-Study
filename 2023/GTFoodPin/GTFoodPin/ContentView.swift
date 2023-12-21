7bbda9ce40e1c6a251ffe3683035909c1c052a64
augus <venn221@gmail.com>
2023-12-21 16:36:56 +0800

    add: first add food pin app

---
+++ 2023/GTFoodPin/GTFoodPin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GTFoodPin
+//
+//  Created by Augus Venn on 2023/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2023/GTFoodPin/GTFoodPin/GTFoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTFoodPinApp.swift
+//  GTFoodPin
+//
+//  Created by Augus Venn on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GTFoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
