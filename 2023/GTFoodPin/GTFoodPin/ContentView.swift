58a0a19cc067fae08456eba01815a9a9739626c8
weiniu <venn221@gmail.com>
2023-12-22 02:18:08 +0800

    add: add click simulate view builder

--- 2023/GTFoodPin/GTFoodPin/ContentView.swift
+++ 2023/GTFoodPin/GTFoodPin/ContentView.swift
@@ -7,17 +7,73 @@
 
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    @State var isShowOverlay: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        SNClickSimulateView {
+            ItemView()
         }
-        .padding()
+
+    }
+    
+}
+
+
+struct SNClickSimulateView<Content: View>: View {
+    
+    let content: Content
+    @State var showOverlay: Bool = false
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        content
+            .background(.white)
+            .onTapGesture {
+                print("onTapGesture")
+                withAnimation(Animation.linear(duration: 0.1).delay(0.1)) {
+                    self.showOverlay = false
+                }
+            }
+            .simultaneousGesture(
+                TapGesture()
+                    .onEnded {
+                        print("TapGesture onEnded")
+                        self.showOverlay = true
+                    }
+            )
+            .opacity(self.showOverlay ? 0.3 : 1)
+    }
+    
+}
+
+
+struct ItemView: View {
+    var body: some View {
+        
+        HStack(alignment: .center) {
+            Text("外国人对亚洲人")
+                .frame(width: 150.0)
+                .font(.title)
+//                .foregroundColor(.black)
+                .lineLimit(1)
+            
+            Image("activity_hot_icon_img")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30)
+        }
     }
 }
+
+
 
 #Preview {
     ContentView()
